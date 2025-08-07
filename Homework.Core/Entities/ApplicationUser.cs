ece3e8df7e386612442ac4f8ba99814a05f25f50
Alexandre Kakhiani <kakhianisandro12@gmail.com>
2025-08-07 17:04:56 +0400

    reinstalled packages because previously it wasn't targeting asp.net 8.0
    also added ApplicationUser.cs

---
+++ Homework.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace homework.Homework.Core.Entities
+{
+    /// <summary>
+    /// the user entity. esentially the base of whoever's trying to access the application.
+    /// Inherits stuff from IdentityUser(email field, password n stuff)
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+      
+    }
+}
